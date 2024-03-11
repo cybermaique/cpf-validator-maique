30c4ae70e45a00f68334dabfca5f1329b565e427
maique.silva <maique.silva@e-unicred.com.br>
2024-03-11 13:15:21 -0300

    feat: add cpf-validator function

---
+++ src/validateCPF.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.validateCPF = void 0;
+const validateCPF = (cpf) => {
+    cpf = cpf.replace(/\D/g, "");
+    if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf))
+        return false;
+    let sum = 0;
+    for (let i = 0; i < 9; i++) {
+        sum += parseInt(cpf.charAt(i)) * (10 - i);
+    }
+    let check = 11 - (sum % 11);
+    if (check === 10 || check === 11)
+        check = 0;
+    if (check !== parseInt(cpf.charAt(9)))
+        return false;
+    sum = 0;
+    for (let i = 0; i < 10; i++) {
+        sum += parseInt(cpf.charAt(i)) * (11 - i);
+    }
+    check = 11 - (sum % 11);
+    if (check === 10 || check === 11)
+        check = 0;
+    if (check !== parseInt(cpf.charAt(10)))
+        return false;
+    return true;
+};
+exports.validateCPF = validateCPF;
